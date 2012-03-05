d71f8a4bfe0a7afd0bf80f61f468b5e2af77a0be
Even Rouault <even.rouault@mines-paris.org>
2012-03-05 21:11:09 +0000

    GTiff: set color interpretation to GCI_PaletteIndex after calling SetColorTable() (#4547)
    git-svn-id: https://svn.osgeo.org/gdal/trunk@24075 f0d54148-0727-0410-94bb-9a71ac55c965

--- gdal/frmts/gtiff/geotiff.cpp
+++ gdal/frmts/gtiff/geotiff.cpp
@@ -1687,6 +1687,7 @@ CPLErr GTiffRasterBand::SetColorTable( GDALColorTable * poCT )
         poGDS->bNeedsRewrite = TRUE;
 
     poGDS->poColorTable = poCT->Clone();
+    eBandInterp = GCI_PaletteIndex;
 
     return CE_None;
 }
