97120f10b61709b1b6f5200256fb79663fc5d5a2
Robert Coup <robert@coup.net.nz>
2012-11-22 17:19:57 +1300

    Expand KMLSuperOverlay tile size to 512 < side < 1024px rather than 200 < side < 400px

--- frmts/kmlsuperoverlay/kmlsuperoverlaydataset.cpp
+++ frmts/kmlsuperoverlay/kmlsuperoverlaydataset.cpp
@@ -733,10 +733,11 @@ KmlSuperOverlayCreateCopy(const char *pszFilename, GDALDataset *poSrcDS,
     int tilexsize;
     int tileysize;
     // Let the longer side determine the max zoom level and x/y tilesizes.
-    if (xsize >= ysize)
+    // tilesizes will be between 512 and 1024px
+    if ( xsize >= ysize )
     {
         double dtilexsize = xsize;
-        while (dtilexsize > 400)  // calculate x tile size
+        while (dtilexsize > 1024) //calculate x tile size
         {
             dtilexsize = dtilexsize / 2;
             maxzoom++;
@@ -747,7 +748,7 @@ KmlSuperOverlayCreateCopy(const char *pszFilename, GDALDataset *poSrcDS,
     else
     {
         double dtileysize = ysize;
-        while (dtileysize > 400)  // calculate y tile size
+        while (dtileysize > 1024) //calculate y tile size
         {
             dtileysize = dtileysize / 2;
             maxzoom++;
