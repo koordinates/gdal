715dc2c554c17197c47caab4739c12c0b013a81a
Craig de Stigter <craig.destigter@koordinates.com>
2014-12-10 14:14:20 +1300

    gdal_calc: convert data types earlier so output numbers don't get wrapped to the input data-type.
    Includes a test

--- gdal/swig/python/scripts/gdal_calc.py
+++ gdal/swig/python/scripts/gdal_calc.py
@@ -152,6 +152,8 @@ def doit(opts, args):
         else:
             myOutType=opts.type
 
+        numpyOutType = gdalnumeric.codes[gdal.GetDataTypeByName(myOutType)]
+
         # create file
         myOutDrv = gdal.GetDriverByName(opts.format)
         myOut = myOutDrv.Create(
@@ -258,6 +260,9 @@ def doit(opts, args):
                     myval=gdalnumeric.BandReadAsArray(myFiles[i].GetRasterBand(myBandNo),
                                           xoff=myX, yoff=myY,
                                           win_xsize=nXValid, win_ysize=nYValid)
+
+                    if numpyOutType != myval.dtype:
+                        myval = myval.astype(numpyOutType)
 
                     # fill in nodata values
                     myNDVs=1*numpy.logical_or(myNDVs==1, myval==myNDV[i])
