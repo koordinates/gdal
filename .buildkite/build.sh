44647d8035fc8bcc9f6e7a836dd7c8826c828085
Craig de Stigter <craig@destigter.nz>
2021-06-10 16:56:23 +1200

    Buildkite CI

---
+++ .buildkite/build.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+set -eu
+
+if [ -n "${KX_BUILD_DEBUG-}" ]; then
+  echo "Enabling script debugging..."
+  set -x
+fi
+
+export TIMEFORMAT='🕑 %1lR'
+export DEBEMAIL=support@koordinates.com
+export DEBFULLNAME="Koordinates CI Builder"
+
+echo "Updating changelog..."
+
+DEB_BASE_VERSION=$(cat gdal/VERSION)
+DEB_VERSION="${DEB_BASE_VERSION}+ci${BUILDKITE_BUILD_NUMBER}-$(git show -s --date=format:%Y%m%d --format=git%cd.%h)"
+echo "Debian Package Version: ${DEB_VERSION}"
+
+if [ -n "${BUILDKITE_AGENT_ACCESS_TOKEN-}" ] ; then 
+  buildkite-agent meta-data set deb-base-version "$DEB_BASE_VERSION"
+  buildkite-agent meta-data set deb-version "$DEB_VERSION"
+
+  echo -e ":debian: Package Version: \`${DEB_VERSION}\`" \
+      | buildkite-agent annotate --style info --context deb-version
+fi
+
+time docker run \
+  -v "$(pwd):/src" \
+  -w "/src/gdal" \
+  -e DEBEMAIL \
+  -e DEBFULLNAME \
+  "${ECR}/ci-tools:latest" \
+    dch --distribution bionic --newversion "${DEB_VERSION}" "Koordinates CI build of ${BUILDKITE_COMMIT}: branch=${BUILDKITE_BRANCH} tag=${BUILDKITE_TAG-}"
+
+BUILD_CONTAINER="build-${BUILDKITE_JOB_ID}"
+
+echo "--- Building debian package ..."
+# Uses a docker volume for ccache
+time docker run \
+  --name "${BUILD_CONTAINER}" \
+  -v "$(pwd):/kx/source" \
+  -v "ccache:/ccache" \
+  -e CCACHE_DIR=/ccache \
+  -w "/kx/source/gdal" \
+  "${ECR}/bionicbuild:latest" \
+    /kx/buildscripts/build_binary_package.sh -uc -us
+
+echo "--- Signing debian archives ..."
+time docker run \
+  -v "$(pwd):/src" \
+  -e "GPG_KEY=${APT_GPG_KEY}" \
+  -w "/src" \
+  "${ECR}/ci-tools:latest" \
+    sign-debs "/src/build-bionic/*.deb"
+
+echo "--- Running tests ..."
+TEST_IMAGE="test-${BUILDKITE_JOB_ID}"
+docker commit "${BUILD_CONTAINER}" "${TEST_IMAGE}"
+docker rm "${BUILD_CONTAINER}"
+R=0
+time docker run --rm -i \
+  -v "$(pwd):/src" \
+  -w "/src/autotest" \
+  "${TEST_IMAGE}" \
+  /bin/bash -exs << EOF || R=$?
+DEBIAN_FRONTEND=noninteractive apt-get update -q
+DEBIAN_FRONTEND=noninteractive apt-get install -y curl
+curl --silent https://bootstrap.pypa.io/get-pip.py 'pip<19' | python3 -
+pip install 'pytest'
+
+DEBIAN_FRONTEND=noninteractive dpkg -i ../build-bionic/{gdal-bin,gdal-data,libgdal20,python3-gdal}*.deb
+
+# skip known failures
+rm gcore/rfc30.py
+TRAVIS=YES TRAVIS_BRANCH=ubuntu_1804 pytest -v -k 'not (test_ogr_fgdb_13 or test_vsisync or test_vsis3_init or test_vsis3_1 or test_vsis3_cleanup or test_eedai_GOOGLE_APPLICATION_CREDENTIALS)'
+EOF
+
+if [ $R -ne 0 ]; then
+  echo "^^^ +++"
+  echo "⚠️ Errors running GDAL tests ($R). But may be kinda expected. Check them and edit build.sh to skip?"
+else
+  echo "--- ✅ GDAL tests passed!"
+fi
+
