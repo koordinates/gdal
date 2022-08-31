348c05e2803081a30d4803566b3a0f8d28d9af4b
Craig de Stigter <craig@destigter.nz>
2022-08-31 14:39:39 +1200

    Build for arm64

--- .buildkite/build.sh
+++ .buildkite/build.sh
@@ -10,18 +10,20 @@ export TIMEFORMAT='🕑 %1lR'
 export DEBEMAIL=support@koordinates.com
 export DEBFULLNAME="Koordinates CI Builder"
 
+export ARCH
+
 echo "Updating changelog..."
 
 DEB_BASE_VERSION=$(cat gdal/VERSION)
 DEB_VERSION="${DEB_BASE_VERSION}+kx-ci${BUILDKITE_BUILD_NUMBER}-$(git show -s --date=format:%Y%m%d --format=git%cd.%h)"
 echo "Debian Package Version: ${DEB_VERSION}"
 
-if [ -n "${BUILDKITE_AGENT_ACCESS_TOKEN-}" ] ; then 
+if [ -n "${BUILDKITE_AGENT_ACCESS_TOKEN-}" ]; then
   buildkite-agent meta-data set deb-base-version "$DEB_BASE_VERSION"
   buildkite-agent meta-data set deb-version "$DEB_VERSION"
 
-  echo -e ":debian: Package Version: \`${DEB_VERSION}\`" \
-      | buildkite-agent annotate --style info --context deb-version
+  echo -e ":debian: Package Version: \`${DEB_VERSION}\`" |
+    buildkite-agent annotate --style info --context deb-version
 fi
 
 time docker run \
@@ -30,20 +32,25 @@ time docker run \
   -e DEBEMAIL \
   -e DEBFULLNAME \
   "${ECR}/ci-tools:latest" \
-    dch --distribution focal --newversion "${DEB_VERSION}" "Koordinates CI build of ${BUILDKITE_COMMIT}: branch=${BUILDKITE_BRANCH} tag=${BUILDKITE_TAG-}"
+  dch --distribution focal --newversion "${DEB_VERSION}" "Koordinates CI build of ${BUILDKITE_COMMIT}: branch=${BUILDKITE_BRANCH} tag=${BUILDKITE_TAG-}"
 
-BUILD_CONTAINER="build-${BUILDKITE_JOB_ID}"
+BUILD_CONTAINER="build-${BUILDKITE_JOB_ID}-${ARCH}"
 
-echo "--- Building debian package ..."
-# Uses a docker volume for ccache
+echo "--- Building debian package for ${ARCH}..."
+DOCKER_PLATFORM="linux/${ARCH}"
+if [ "${ARCH}" == "arm64" ]; then
+  DOCKER_PLATFORM="linux/arm64/v8"
+fi
+
 time docker run \
   --name "${BUILD_CONTAINER}" \
+  --platform="${DOCKER_PLATFORM}" \
   -v "$(pwd):/kx/source" \
   -v "ccache:/ccache" \
   -e CCACHE_DIR=/ccache \
   -w "/kx/source/gdal" \
   "${ECR}/focalbuild:latest" \
-    /kx/buildscripts/build_binary_package.sh -uc -us
+  /kx/buildscripts/build_binary_package.sh -uc -us
 
 echo "--- Signing debian archives ..."
 time docker run \
@@ -51,10 +58,10 @@ time docker run \
   -e "GPG_KEY=${APT_GPG_KEY}" \
   -w "/src" \
   "${ECR}/ci-tools:latest" \
-    sign-debs "/src/build-focal/*.deb"
+  sign-debs "/src/build-focal/*.deb"
 
 echo "--- Running tests ..."
-TEST_IMAGE="test-${BUILDKITE_JOB_ID}"
+TEST_IMAGE="test-${BUILDKITE_JOB_ID}-${ARCH}"
 docker commit "${BUILD_CONTAINER}" "${TEST_IMAGE}"
 docker rm "${BUILD_CONTAINER}"
 R=0
@@ -62,7 +69,7 @@ time docker run --rm -i \
   -v "$(pwd):/src" \
   -w "/src/autotest" \
   "${TEST_IMAGE}" \
-  /bin/bash -exs << EOF || R=$?
+  /bin/bash -exs <<EOF || R=$?
 DEBIAN_FRONTEND=noninteractive apt-get update -q
 DEBIAN_FRONTEND=noninteractive apt-get install -y curl
 curl --silent https://bootstrap.pypa.io/get-pip.py 'pip<19' | python3 -
@@ -81,4 +88,3 @@ if [ $R -ne 0 ]; then
 else
   echo "--- ✅ GDAL tests passed!"
 fi
-
