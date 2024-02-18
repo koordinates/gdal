d01ccea431a668d720eba57faa3e4117e4332764
Even Rouault <even.rouault@spatialys.com>
2024-02-18 19:42:11 +0100

    JP2OpenJPEG: CreateCopy(): limit number of resolutions taking into account minimum block width/height (fixes #9236)

--- autotest/gdrivers/jp2openjpeg.py
+++ autotest/gdrivers/jp2openjpeg.py
@@ -3842,3 +3842,17 @@ def test_jp2openjpeg_reversible_quality_not_100():
     ds = None
 
     gdaltest.jp2openjpeg_drv.Delete(filename)
+
+
+###############################################################################
+# Test fix for https://github.com/OSGeo/gdal/issues/9236
+
+
+def test_jp2openjpeg_limit_resolution_count_from_image_size(tmp_vsimem):
+
+    filename = str(tmp_vsimem / "out.jp2")
+    assert gdal.Translate(filename, "data/byte.tif", width=1024, height=7)
+
+    # Check number of resolutions
+    ret = gdal.GetJPEG2000StructureAsString(filename, ["ALL=YES"])
+    assert '<Field name="SPcod_NumDecompositions" type="uint8">2</Field>' in ret
