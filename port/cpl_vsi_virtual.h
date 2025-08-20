a70b3064c0a466bfe2c7a45fdeaffc7eeb8593e6
Craig de Stigter <craig@destigter.nz>
2025-08-20 14:49:12 +1200

    VSI archive performance improvements
    Traversal of large archives (many entries, especially in a large
    hierarchy) can be slow.
    
    This change massively improves performance of random lookups by path
    within an archive, as well as traversal using VSIReadDirRecursive. The
    main change is to avoid full traversal of the VSIArchiveContent::entries
    array for each path lookup.
    
    I achieved this by building an index mapping each directory entry
    to the indices of its children in the entries list, and using that index
    to speed up lookups by path.
    
    This also has a massive perf improvement for
    VSIReadDirRecursive since it works by calling ReadDirEx for each
    subdirectory (which previously meant looking at all entries). Now it
    uses the directory index to immediately jump to where the directory is
    in the entries list, avoiding visiting other entries.
    
    On my laptop, ReadDirRecursive on a zip file containing 600 dirs each
    containing 600 files:
    
    * previous master gdal: 728 seconds
    * after this change: 4.2 seconds

--- port/cpl_vsi_virtual.h
+++ port/cpl_vsi_virtual.h
@@ -446,6 +446,9 @@ typedef struct
     GIntBig nModifiedTime;
 } VSIArchiveEntry;
 
+// Store list of child indices for each directory
+using DirectoryChildren = std::vector<int>;
+
 class VSIArchiveContent
 {
   public:
@@ -453,6 +456,10 @@ class VSIArchiveContent
     vsi_l_offset nFileSize = 0;
     int nEntries = 0;
     VSIArchiveEntry *entries = nullptr;
+
+    std::map<std::string, DirectoryChildren> dirIndex{};
+
+    VSIArchiveContent() = default;
 
     ~VSIArchiveContent();
 };
