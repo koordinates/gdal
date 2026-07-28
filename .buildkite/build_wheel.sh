6c2bbc94d37acf7d3174e0a2b4ac2b5d0b344ccf
Craig de Stigter <craig.destigter@koordinates.com>
2026-07-28 13:34:27 +1200

    Substitute @CMAKE_OSX_SYSROOT@ in wheel build
    New placeholder in GDAL 3.13's setup.py.in. Left unsubstituted it passes
    '-isysroot @CMAKE_OSX_SYSROOT@' to g++, hiding the multiarch include dir.
    Empty makes the guarding 'if' falsy, so the flags are skipped on Linux.

--- .buildkite/build_wheel.sh
+++ .buildkite/build_wheel.sh
@@ -78,6 +78,7 @@ sed \
   -e "s|@GDAL_LIB_DIR@||g" \
   -e "s|@GDAL_LIB_OUTPUT_NAME@|gdal|g" \
   -e "s|@GNM_ENABLED@|True|g" \
+  -e "s|@CMAKE_OSX_SYSROOT@||g" \
   -e "s|extras_require={'numpy': \['numpy > 1.0.0'\]}|install_requires=['numpy>=2.0,<3']|g" \
   -e "s|gdal_python_version = \[int(x) for x in bindings_version.split('.')\]|gdal_python_version = [int(x) for x in bindings_version.split('.')[:3]]|g" \
   -e "s|if library_version_num < gdal_python_version:|if False:|g" \
